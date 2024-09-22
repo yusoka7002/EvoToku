49431c01fc223a5fc6546e5dec866e0b2a1b5a4b
yusoka7002 <163638140+yusoka7002@users.noreply.github.com>
2024-09-22 10:48:34 +0900

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,17 +1,12 @@
 const hre = require("hardhat");
-
 async function main() {
   const votingContract = await hre.ethers.getContractFactory("Voting");
   const deployedVotingContract = await votingContract.deploy();
-
   console.log(`Contract Address deployed: ${deployedVotingContract.target}`);
-
 }
-
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-
-// Contract Address deployed: 0x10a5Cbb7F4D92BaF73e6C17099F1d1FDCD1e96A1
-// Contract Address verified: https://sepolia.etherscan.io/address/0x10a5Cbb7F4D92BaF73e6C17099F1d1FDCD1e96A1#code
+// Contract Address deployed: 0x8aEe7e8C57B30048979599f7Db98c4288C4F3714
+// Contract Address verified: https://sepolia.etherscan.io/address/0x8aEe7e8C57B30048979599f7Db98c4288C4F3714#code
